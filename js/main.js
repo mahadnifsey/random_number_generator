4041108dafc885d97c405ab259c3465ea9fe86f5
mahadnifsey <mahadnifsey@gmail.com>
2022-04-27 03:53:03 +0100

    updated project with working script

--- js/main.js
+++ js/main.js
@@ -1,34 +1,10 @@
-let raceNumber = Math.floor(Math.random() * 1000);
+let button = document.querySelector("button");
+let answer = document.querySelector("#main");
 
-let registeredEarly = true;
+button.addEventListener("click", function() {
 
-let runnersAge = 20;
+let raceNumber = Math.floor(Math.random() * 100);
 
-// Checking Runners who registered Early & Adults.
-if (registeredEarly && runnersAge > 18) {
-    raceNumber += 1000;
-}
+main.innerHTML = raceNumber;
 
-// Checking Runners age & registration time to determine racetime.
-
-// For adult runners who registered early.
-if (registeredEarly && runnersAge > 18) {
-    console.log(`Racenumber ${raceNumber += 1000}, you will race at 9.30am.`);
-// For adult runners who registered late.
-} else if (!registeredEarly && runnersAge > 18) {
-    console.log(`Racenumber ${raceNumber}, you will race at 11:00am.`);
-// For runners younger than 18.
-} else if (runnersAge < 18) {
-    console.log(`Racenumber ${raceNumber}, you will race at 12.30pm.`);
-// For runners who are 18 years old.
-} else { 
-    console.log("Please see the registration desk please!");
-}
-
-function sayThanks(name) {
-    console.log(`Thank you for your purchase ${name}! We appreciate your business.`);
-  }
-  
-  sayThanks ("Cole");
-  
-  
+});
